397b5add7c2111b5325f2da6eb0a4774be7e522b
ryoung06 <110110411+ryoung06@users.noreply.github.com>
2026-07-21 16:42:17 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  define: {
+    'process.env.NODE_ENV': '"production"',
+    'process.env': '({})',
+    process: '({env:{NODE_ENV:"production"}})',
+  },
   build: {
+    copyPublicDir: true,
     lib: {
       entry: 'src/main.tsx',
       name: 'RisingTideWidget',
